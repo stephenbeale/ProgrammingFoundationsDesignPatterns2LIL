0922986da2d640a836b3decc4eb66d58f042987a
Steve Beale <beale.stephen@gmail.com>
2024-08-28 08:49:29 +0100

    First commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class FlyWithWings: FlyBehaviour
+{
+    public void fly()
+    {
+        Console.WriteLine("I'm flying!!");
+    }
+}
